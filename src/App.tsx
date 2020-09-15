8d67761e2f0c18ebbc0515c33c3440aa813d6f47
genghai <genghai@xsky.com>
2020-09-16 01:18:58 +0800

    feat button components.
    Signed-off-by: genghai <genghai@xsky.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button ,{ButtonType,ButtonSize} from './components/Button/button'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button> hello</Button>
+        <Button onClick={e=>{console.log('jj')}} btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>hello</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>hello</Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Small} disabled>hello</Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Small}>hello</Button>
+        <Button btnType={ButtonType.Link} href="https://baidu.com" disabled >hello</Button>
+        <Button btnType={ButtonType.Link} href="https://baidu.com"  >hello</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <code>
+          const a = "dd"
+        </code>
         <a
           className="App-link"
           href="https://reactjs.org"
