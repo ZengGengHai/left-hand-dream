7ae2756b68b4d9104eb7bdf4f13fd78449f35444
genghai <genghai@xsky.com>
2020-09-18 01:26:41 +0800

    feat menu and style.
    Signed-off-by: genghai <genghai@xsky.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Button ,{ButtonType,ButtonSize} from './components/Button/button'
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0} onSelect={(index) => {console.log(index)}}>
+          <MenuItem index={0}>link1</MenuItem>
+          <MenuItem index={1} >link2</MenuItem>
+          <MenuItem index={2}>link3</MenuItem>
+        </Menu>
         <Button> hello</Button>
-        <Button onClick={e=>{console.log('jj')}} btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button onClick={e => { console.log('jj') }} btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>hello</Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>hello</Button>
         <Button btnType={ButtonType.Default} size={ButtonSize.Small} disabled>hello</Button>
